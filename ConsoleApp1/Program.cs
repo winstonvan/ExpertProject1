9af81c24cd815da6666a5d47150bf5c8cc220d98
Kevin <kevinjacquesonline@gmail.com>
2020-03-14 15:32:05 -0400

    First commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            System.Console.Out.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
