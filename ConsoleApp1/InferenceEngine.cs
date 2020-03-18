d2f810a3dd16a912fc6a95b68b161332528d26cc
Winston Van <wvan@laurentian.ca>
2020-03-17 20:00:36 -0400

    Fact and Rule classes added. IE needs work.

---
+++ ConsoleApp1/InferenceEngine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class InferenceEngine
+    {
+        private List<Rule> rules;
+        private List<Fact> facts;
+
+        public InferenceEngine()
+        {
+            rules = new List<Rule>();
+
+        }
+
+        //****** RULE FUNCTIONS
+        public void AddRule(Rule rule)
+        {
+            rules.Add(rule);
+        }
+
+        public void DeleteRule(int i) 
+        {
+            rules.RemoveAt(i);
+        }
+
+        public void DeleteAllRules() 
+        {
+            rules.Clear();
+        }
+
+        public void GetRules()
+        {
+            for (int i = 0; i < rules.Count; i++)
+            {
+                Console.WriteLine(rules[i].Print());
+            }   
+        }
+
+        //****** FACT FUNCTIONS
+        public void AddFact(String variable, String op, String value)
+        {
+            facts.Add(new Fact(variable, op, value));
+        }
+
+        public List<Fact> GetFacts()
+        {
+            return facts;
+        }
+    }
+}
---
+++ ConsoleApp1/Rule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	public class Rule
+	{
+		private String ruleName;
+
+		private List<Fact> facts;
+		private Fact result;
+
+		public Rule(String ruleName)
+		{
+			this.ruleName = ruleName;
+			this.facts = new List<Fact>();
+		}
+
+		// get functions
+		public String GetRuleName()
+		{
+			return ruleName;
+		}
+
+		// set functions
+		public void AddCondition(Fact condition)
+		{
+			this.facts.Add(condition);
+		}
+
+		public void SetResult(Fact result)
+		{
+			this.result = result;
+		}
+
+		// print to string
+		public String Print()
+		{
+			String temp = "Rule: " + this.ruleName + "\n";
+
+			for (int i = 0; i < facts.Count; i++)
+			{
+				temp += "Condition " + (i + 1) + ": " + this.facts[i].Print() + "\n";
+            }
+
+			temp += "Result: " + this.result.Print() + "\n";
+			return temp;
+		}
+	}
+}
