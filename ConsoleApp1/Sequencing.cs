ceb0af3ba4be0909fe64d956b44077d49f7088f8
Kevin <kevinjacquesonline@gmail.com>
2020-03-19 00:43:58 -0400

    .

--- ConsoleApp1/Sequencing.cs
+++ ConsoleApp1/Sequencing.cs
@@ -8,22 +8,29 @@ namespace ConsoleApp1
 {
     public class Sequencing
     {
-        public long Bits { get; set; }
+        public int Id { get; set; }
 
         public Sequencing() { }
 
-        public Sequencing (long Bits)
+        public Sequencing (int Id)
         {
-            this.Bits = Bits;
+            this.Id = Id;
         }
 
-        public static Sequencing operator +(Sequencing a, Sequencing b) => new Sequencing(a.Bits | b.Bits);
-        public static Sequencing operator -(Sequencing a, Sequencing b) => new Sequencing(a.Bits & b.Bits);
+        public static Sequencing operator +(Sequencing x, Sequencing y)
+        {
+            return new Sequencing(x.Id | y.Id);
+        }
+
+        public static Sequencing operator -(Sequencing x, Sequencing y)
+        {
+            return new Sequencing(x.Id & y.Id);
+        }
 
 
         public override string ToString()
         {
-            return Bits.ToString();
+            return Id.ToString();
         }
     }
 }
